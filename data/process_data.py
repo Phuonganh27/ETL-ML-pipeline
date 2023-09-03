adeea4ad3f70bec64b8e58d4d9b24cde72e651ef
Phuonganh <phuonganhdang@Phuongs-MacBook-Air.local>
2023-09-03 16:12:58 +0900

    Fix bug: add error handling for ValueError Tablename already exists

--- data/process_data.py
+++ data/process_data.py
@@ -75,7 +75,7 @@ def save_data(df, database_filename):
         None
     """
     engine = create_engine(f'sqlite:///{database_filename}')
-    df.to_sql('CleanData', engine, index=False)
+    df.to_sql('CleanData', engine, index=False, if_exists='replace')
 
 
 def main():
